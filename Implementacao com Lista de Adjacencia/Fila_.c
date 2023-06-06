546e2a5c41a852710c6b452327be6f3a928bbb42
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2023-06-06 13:15:34 -0300

    (Linguagem C)

---
+++ Implementacao com Lista de Adjacencia/Fila_.c
@@ -0,0 +1,168 @@
+/*
+    @ Autor:
+        * Joao Victor do Rozario Recla.
+    @ Atuacao:
+        * Estudante.
+    @ Instituicao:
+        * UFES   - Universidade Federal do Espirito Santo.
+        * CEUNES - Centro universitario Norte do Espirito Santo.
+    
+    Este arquivo contem a implementacao de metodos basicos para
+    criar e deletar filas de vertices, criar, inserir e remover
+    estruturas de vertices.
+*/
+
+#include "Grafos_.h"
+
+
+
+/*<<===================================================================================>>*/
+/*<<===================================================================================>>*/
+
+/*
+    Metodo responsavel por criar e
+    inicializar a estrutura de uma fila.
+
+    @ Entrada:
+    + ( void ).
+
+    @ Saida:
+    - Fila [ FILA ].
+*/
+Queue *Criar_Fila_(){
+
+    Queue *Fila = (Queue*) malloc(sizeof(Queue));
+
+    if(Fila){
+        Fila->Size  = 0;
+        Fila->First = NULL;
+        Fila->Last  = NULL;
+    }
+    else SEM_MEMORIA
+
+    return Fila;
+}
+
+
+/*
+    Metodo responsavel por inserir a estrutura
+    de um vertice no final de uma fila.
+    
+    @ Entrada:
+    + Fila [ FILA ].
+    + Identificador do vertice [ IDENTIFICADOR ].
+
+    @ Saida:
+    - ( void ).
+*/
+void Inserir_Vertice_Fila_(Queue *Fila, int Identificador){
+    
+    if(Fila){
+
+        // Verifica se a fila esta vazia.
+        if(Fila->Size == 0){
+
+            Fila->First = Criar_Vertice_(Identificador, 1);
+            Fila->Last  = Fila->First;
+        }
+        else{
+            
+            Fila->Last->Prox = Criar_Vertice_(Identificador, 1);
+            Fila->Last       = Fila->Last->Prox;
+        }
+
+        Fila->Size++;
+    }
+    else SEM_FILA
+}
+
+
+/*
+    Metodo responsavel por remover a estrutura de
+    um vertice, do inicio de uma fila, retornando
+    seu identificador.
+    
+    @ Entrada:
+    + Fila [ FILA ].
+
+    @ Saida:
+    - Identificador do vertice [ IDENTIFICADOR ].
+*/
+int Remover_Vertice_Fila_(Queue *Fila){
+
+    int Identificador = 0;
+
+    if(Fila){
+        
+        Vertice *Aux  = Fila->First;
+        Fila->First   = Aux->Prox;
+        Identificador = Aux->No;
+
+        Fila->Size--;
+        free(Aux);  // Deleta a estrutura do vertice.
+    }
+    else SEM_FILA
+    
+    return Identificador;
+}
+
+
+/*
+    Metodo responsavel por imprimir a estrutura de
+    uma fila exibindo todos os seus vertices.
+
+    @ Entrada:
+    + Fila [ FILA ].
+
+    @ Saida:
+    - ( void ).
+*/
+void Imprimir_Fila_(Queue *Fila){
+
+    if(Fila){
+
+        Vertice *Aux = Fila->First;
+
+        // Loop: Imprimir vertices.
+        printf("|----|");
+        while(Aux){
+
+            printf(" %02d", Aux->No+1);
+            Aux = Aux->Prox;
+        }
+        printf("\n");
+    }
+    else SEM_FILA
+}
+
+
+/*
+    Metodo responsavel por deletar uma fila
+    removendo cada estrutura de um vertice.
+
+    @ Entrada:
+    + Fila [ FILA ].
+
+    @ Saida:
+    - ( void ).
+*/
+void Deletar_Fila_(Queue *Fila){
+
+    if(Fila){
+
+        Vertice *Aux;
+
+        // Loop: Deletar vertices.
+        while(Fila->First){
+
+            Aux         = Fila->First;
+            Fila->First = Fila->First->Prox;
+            free(Aux);
+        }
+
+        free(Fila); // Deleta a estrutura da fila.
+    }
+}
+
+/*<<===================================================================================>>*/
+/*<<===================================================================================>>*/
---
+++ Implementacao com Lista de Adjacencia/Grafos_.h
@@ -0,0 +1,104 @@
+#ifndef GRAFOS_H
+#define GRAFOS_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+#define SIM         1           // True.
+#define NAO         0           // False.
+#define SEM_ARESTA  0
+#define INFINITO    INT_MAX/2   // Valor maximo.
+#define SEM_FILA    printf("\n|----| [0_0] Voce nao tem uma fila.\n");
+#define SEM_GRAFO   printf("\n|----| [0_0] Voce nao tem um grafo.\n");
+#define SEM_MEMORIA printf("\n|----| [x_x] Sem memoria disponivel !\n");
+
+
+
+/*<<===================================================================================>>*/
+/*<<==================================>> ESTRUTURA <<==================================>>*/
+/*<<===================================================================================>>*/
+
+/*
+    Estrutura representativa de um vertice.
+*/
+typedef struct Vertice{
+    int No;                 // Identificador do vertice.
+    int Peso;               // Peso de uma aresta.
+    struct Vertice *Prox;   // Ponteiro para vertices adjacentes.
+}Vertice;
+
+/*
+    Estrutura representativa de um grafo
+    usando lista de adjacencias.
+*/
+typedef struct Grafo{
+    int Qnt_V;      // Quantidade de vertices.
+    int Qnt_E;      // Quantidade de arestas.
+    Vertice *Adj;   // Lista de adjacencias.
+}Grafo;
+
+Grafo *Criar_Grafo_(int N);
+Vertice *Criar_Vertice_(int Identificador, int Peso);
+void Inserir_Aresta_(Grafo *G, int V, int W, int Peso);
+void Remover_Aresta_(Grafo *G, int V, int W);
+void Imprimir_Grafo_(Grafo *G);
+void Deletar_Grafo_(Grafo *G);
+
+
+
+/*<<===================================================================================>>*/
+/*<<===============================>>  ESTRUTURA FILA <<===============================>>*/
+/*<<===================================================================================>>*/
+
+/*
+    Estrutura representativa
+    de uma fila de vertices.
+*/
+typedef struct{
+    int Size;       // Tamanho da fila.
+    Vertice *First; // Ponteiro para o primeiro vertice da fila.
+    Vertice *Last;  // Ponteiro para o ultimo vertice da fila.
+}Queue;
+
+Queue *Criar_Fila_();
+void Inserir_Vertice_Fila_(Queue *Fila, int Identificador);
+int Remover_Vertice_Fila_(Queue *Fila);
+void Imprimir_Fila_(Queue *Fila);
+void Deletar_Fila_(Queue *Fila);
+
+
+
+/*<<===================================================================================>>*/
+/*<<=============================>> PERCURSOS EM GRAFOS <<=============================>>*/
+/*<<===================================================================================>>*/
+
+void Imprimir_Percurso_em_Largura_(Grafo *G, int Raiz);
+int  Verificar_Grafo_Bipartido_(Grafo *G, int Raiz);
+void Percurso_em_Profundidade_(Grafo *G, int Raiz, int **Alcancados, Queue *Explorados);
+void Imprimir_Percurso_em_Profundidade_(Grafo *G, int Raiz);
+
+
+
+/*<<===================================================================================>>*/
+/*<<============================>> UTILITARIOS EM GRAFOS <<============================>>*/
+/*<<===================================================================================>>*/
+
+/*
+    Estrutura representativa de uma aresta.
+*/
+typedef struct Aresta{
+    int V;      // Vertice de partida.
+    int W;      // Vertice de chegada.
+	int Peso;   // Peso da aresta.
+}Aresta;
+
+Aresta *Criar_Vetor_Arestas_(Grafo *G);
+int    Calcular_Grau_(Grafo *G, int V);
+void   Imprimir_Caminho_(int *Predecessor, int Raiz, int V);
+void   Ordenar_Arestas___Insertion_Sort_(Aresta *Arestas, int Tam);
+
+#endif
+
+/*<<===================================================================================>>*/
+/*<<===================================================================================>>*/
