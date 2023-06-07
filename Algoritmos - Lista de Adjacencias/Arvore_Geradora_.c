d555aa247078e4b331c1cf46093f27aefa6acb54
JoaoVictorRR-GitHub <joaovictor.rr@hotmail.com>
2023-06-07 00:28:31 -0300

    (Linguagem C)

---
+++ Algoritmos - Lista de Adjacencias/Arvore_Geradora_.c
@@ -0,0 +1,21 @@
+/*
+    @ Autor:
+        * Joao Victor do Rozario Recla.
+    @ Atuacao:
+        * Estudante.
+    @ Instituicao:
+        * UFES   - Universidade Federal do Espirito Santo.
+        * CEUNES - Centro universitario Norte do Espirito Santo.
+    
+    Este arquivo contem a implementacao de algoritmos avancados para
+    encontrar a representacao de uma arvore geradora minima de um
+    grafo usando lista de adjacencias.
+*/
+
+#include "Grafos_.h"
+
+
+
+/*<<===================================================================================>>*/
+/*<<==============================>> ALGORITMO DE PRIM <<==============================>>*/
+/*<<===================================================================================>>*/
